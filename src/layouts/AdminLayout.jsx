328ec292f3e8e8c3daa4b37d0702d46efe699a7c
rreennn <kerenkaren1704@gmail.com>
2026-03-11 04:05:02 +0700

    fix case sensitive import

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/admin/Sidebar";
+import Sidebar from "../components/Admin/Sidebar"
 
 function AdminLayout() {
   return (
